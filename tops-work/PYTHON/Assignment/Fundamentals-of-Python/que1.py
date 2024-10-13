e74f53a83997b34d16e7a0cd0abef5c875f51b88
Higanesh <ganesh250520@gmail.com>
2024-10-13 22:56:37 +0530

    first python program

---
+++ tops-work/PYTHON/Assignment/Fundamentals-of-Python/que1.py
@@ -0,0 +1,13 @@
+"""
+Que1. Write a Python program to check if a number is positive, negative or
+zero. 
+"""
+
+number = -0
+if number > 0:
+    print("Number is POSITIVE")
+elif number < 0:
+     print("Number is NEGATIVE")
+else:
+    print("Number is ZERO")
+
